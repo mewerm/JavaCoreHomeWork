a82a3456a5e9585f5a23116fc8605e7e1c5ba6a9
Maxim Meshcheryakov <25-nv-86@mail.ru>
2022-04-19 19:39:08 +0300

    Новый коммит, разбираюсь с гитом

--- src/main/java/Work3/HomeWorkThree.java
+++ src/main/java/Work3/HomeWorkThree.java
@@ -3,5 +3,6 @@ package Work3;
 public class HomeWorkThree {
     public static void main(String[] args) {
         System.out.println("WTF? ПОЧЕМУ ГИТ ТАКОЙ СЛОЖНЫЙ ");
+        System.out.println("Чисто ради коммита");
     }
 }
