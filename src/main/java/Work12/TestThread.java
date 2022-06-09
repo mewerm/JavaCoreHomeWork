14586c20a796470cd36f18a36b4aff1c51ec876d
Maxim <25-nv-86@mail.ru>
2022-06-09 16:22:48 +0300

    Домашняя работа 12

---
+++ src/main/java/Work12/TestThread.java
@@ -0,0 +1,14 @@
+package Work12;
+
+
+public class TestThread {
+    public static void main(String[] args) {
+
+        ThreadOneTwo r = new ThreadOneTwo();
+        r.testMethod1();
+        r.testMethod2();
+        System.out.println("MAIN END ");
+
+
+    }
+}
